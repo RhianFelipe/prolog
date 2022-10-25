9a9f382fe774bc0322e8de937ec49d802bbe3130
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2022-10-24 22:36:57 -0300

    Update arvoreGen.pl

--- arvoreGen.pl
+++ arvoreGen.pl
@@ -1,4 +1,4 @@
-
+% TO FICANDO MALUCO ME AJUDEM
 % BASE DE CONHECIMENTO
 
 homem(jose).
@@ -11,9 +11,79 @@ mulher(silvia).
 mulher(gertudres).
 mulher(ana).
 
+
+%pai(X,Y).
 %Jose é pai de joao
 pai(jose,joao).
 pai(jose,claudia).
 %mae(X,claudia) == X = silvia
 mae(silvia,joao).
 mae(silvia,claudia).
+
+
+%Regras
+%------
+
+irmao(X,Y):- %se
+    homem(X),
+    pai(P,X),
+    pai(P,Y),
+    not(X = Y).
+
+irmao(X,Y):- %se
+    homem(X),
+    mae(P,X),
+    mae(P,Y),
+    not(X = Y).
+    
+irma(X,Y):- %se
+    mulher(X),
+    pai(P,X),
+    pai(P,Y),
+    not(X = Y).
+
+irma(X,Y):- %se
+    mulher(X),
+    mae(P,X),
+    mae(P,Y),
+    not(X = Y).
+
+pais(X,Pai,Mae):- %se
+    homem(Pai),
+    mulher(Mae),
+    pai(Pai,X),
+    mae(Mae,X),
+    !. % cut impede o backtracking
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%1) Atividade
+
+
+
+gato(tom).
+rato(jerry).
+passaro(piupiu).
+minhoca(moli).
+
+
+animal(X):-
+    gato(X).
+%...
+
+goste(X,Y):-
+    	passaro(X),
+    	minhoca(Y),
+    
+    
+come(X,Y):-
+    gosta(X,Y)
+
+
+
+
+
+
+
